670f8a8b8ae18630bbce15b10a96613d4682beab
rubisselljs-droid <rubisselljs@gmail.com>
2026-08-15 17:12:05 -0600

    sencillo pero algo es algo

--- Practica/src/components/HelloWorld.vue
+++ Practica/src/components/HelloWorld.vue
@@ -1,5 +1,19 @@
-<script setup lang="ts"></script>
+
+
+<script setup lang="ts">
+import { ref } from "vue"; // usamos esto para poder usar la reactividad de vue 3
+
+  let nombre = ref<string>("");
+
+
+</script>
 
 <template>
    <h1>HOLA MUNDO</h1>
+
+
+   <h1> Hola mi nombre es : {{ nombre }}</h1>
+
+
+   <input v-model="nombre" type="text" placeholder="Ingresa tu nombre" />
 </template>
