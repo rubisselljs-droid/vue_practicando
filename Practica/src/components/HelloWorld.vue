4c91689b6067782f14a4c5dc4b76b9fdf679def0
rubisselljs-droid <rubisselljs@gmail.com>
2026-08-15 17:35:36 -0600

    guardando nombre con un button

--- Practica/src/components/HelloWorld.vue
+++ Practica/src/components/HelloWorld.vue
@@ -1,9 +1,16 @@
 
 
 <script setup lang="ts">
-import { ref } from "vue"; // usamos esto para poder usar la reactividad de vue 3
+import { ref } from "vue"; 
+// usamos esto para poder usar la reactividad de vue 
+// ref es una funcion que nos permite crear variables reactivas, es decir
+//  que cuando cambian su valor, la vista se actualiza automaticamente 
+// ES COMO EL RENDER DE REACT, PERO EN VUE 
 
   let nombre = ref<string>("");
+ function Saludar(){
+   console.log('hola ' + nombre.value); 
+ }
 
 
 </script>
@@ -13,7 +20,11 @@ import { ref } from "vue"; // usamos esto para poder usar la reactividad de vue 
 
 
    <h1> Hola mi nombre es : {{ nombre }}</h1>
-
-
+    
+    <!-- v-model conecta el input con la variable nombre, de esta manera cuando el usuario--> 
+    <!--escriba en el input, la variable nombre se actualiza automaticamente -->
    <input v-model="nombre" type="text" placeholder="Ingresa tu nombre" />
+
+
+   <button @click="Saludar">Saludar</button>
 </template>
