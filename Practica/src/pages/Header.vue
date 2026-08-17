837a1065a7abebbad213d77d68e0759c5a11ce7f
rubisselljs-droid <rubisselljs@ejemplo.com>
2026-08-17 13:48:25 -0600

    estilos

--- Practica/src/pages/Header.vue
+++ Practica/src/pages/Header.vue
@@ -12,15 +12,28 @@ const count = ref(0)
 </script>
 
 <template>
-  <header class="bg-primario flex">
-    <div class="flex justify-between items-center space-x-2.5">
+  <header class="bg-primario">
+    <div class="flex justify-between items-center px-10">
       <div class="border-3">
         <img :src="Logo" alt="" class="w-20" />
       </div>
-      <div class="border-3">
-        <a href="">MENU</a>
-        <a href="">EQUIPO</a>
-        <a href="">ESTADOS</a>
+
+      <div class="flex items-center space-x-8">
+        <a
+          href="#"
+          class="text-texto hover:text-blue-600 p-5 transition-colors font-semibold"
+          >MENU</a
+        >
+        <a
+          href="#"
+          class="text-texto hover:text-blue-600 transition-colors font-semibold"
+          >EQUIPO</a
+        >
+        <a
+          href="#"
+          class="text-texto hover:text-blue-600 transition-colors font-semibold"
+          >ESTADOS</a
+        >
       </div>
     </div>
   </header>
