c027fcefd7d44ef7e2562ebd3a3d171ff831d485
rubisselljs-droid <rubisselljs@gmail.com>
2026-08-16 20:32:13 -0600

    recorriendo arreglos simples ya vamos mas o menos eso creo

--- Practica/src/components/Componente.vue
+++ Practica/src/components/Componente.vue
@@ -3,22 +3,46 @@ import { ref } from "vue";
 
 const nombre = ref<string>("");
 
+const nombres = [
+  "EMir ",
+  "Juan",
+  "Pedro",
+  "Maria",
+  "Ana",
+  "Luis",
+  "Carlos",
+  "Sofia",
+  "Diego",
+  "Valentina",
+];
+
 function Eviar() {
   console.log("NOMBRE ENVIADO: " + nombre.value);
 }
 </script>
 
 <template>
-  <header class="bg-black display">
-    <nav class="flex justify-between items-center p-4">
-      <div class="flex space-x-4">
-        <a href="" class="text-white hover:text-gray-300">HOLA </a>
-        <a href="" class="text-white hover:text-gray-300">HOLA </a>
-      </div>
-    </nav>
-
+  <header class="display flex justify-around bg-blue-950 items-center">
     <div>
-      <h1>MENU</h1>
+      <img
+        src="https://i.pinimg.com/1200x/6c/f8/1c/6cf81c52e105a047f149ad8335f12780.jpg"
+        alt="Logo"
+        class="h-12 w-12"
+      />
+    </div>
+    <div class="flex space-x-4">
+      <a href="" class="text-white hover:text-amber-300">INICIO</a>
+      <a href="" class="text-white hover:text-amber-300">NOSOTROS</a>
+      <a href="" class="text-white hover:text-amber-300">SERVICIOS</a>
+      <a href="" class="text-white hover:text-amber-300">CONTACTO</a>
+    </div>
+  </header>
+
+  <section class="flex justify-center bg-amber-300 h-screen">
+    <!--
+    
+        <div>
+      <h1>INGRESE NOMBRE DEL JUGADOR</h1>
 
       <input
         v-model="nombre"
@@ -31,5 +55,15 @@ function Eviar() {
         Enviar
       </button>
     </div>
-  </header>
+    
+    -->
+
+    <p
+      v-for="nombre in nombres"
+      class="flex text-3xl font-bold underline text-white p-2 m-2"
+      :key="nombre"
+    >
+      {{ nombre }}
+    </p>
+  </section>
 </template>
